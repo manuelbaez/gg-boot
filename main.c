0f00e1e1dcfdb74d1f4fa18762e7f8a165c36664
Manuel Baez <manuelbaezbatista@gmail.com>
2023-01-15 15:32:26 -0400

    Remove prints

--- main.c
+++ main.c
@@ -10,14 +10,15 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     EFI_STATUS Status;
 
     EFI_LOADED_IMAGE *AppLoadedImage;
-    Print((CHAR16 *)L"Loading App Image Info With OpenProtocol");
-    Status = uefi_call_wrapper(BS->OpenProtocol, 3,
-                               ImageHandle,
-                               &gEfiLoadedImageProtocolGuid,
-                               &AppLoadedImage,
-                               ImageHandle,
-                               NULL,
-                               EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    // Print((CHAR16 *)L"Loading App Image Info With OpenProtocol");
+    Status =
+        uefi_call_wrapper(BS->OpenProtocol, 3,
+                          ImageHandle,
+                          &gEfiLoadedImageProtocolGuid,
+                          &AppLoadedImage,
+                          ImageHandle,
+                          NULL,
+                          EFI_OPEN_PROTOCOL_GET_PROTOCOL);
 
     // uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
     // Status = uefi_call_wrapper(BS->HandleProtocol, 3,
@@ -56,25 +57,28 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 
     // Print((CHAR16 *)logo);
 
-    Print(L"This isn't Done by iterating fs handles\n");
-    Status = uefi_call_wrapper(BS->OpenProtocol, 5,
-                               AppLoadedImage->DeviceHandle,
-                               &gEfiSimpleFileSystemProtocolGuid,
-                               (VOID **)&RootFileSystemProtocol,
-                               ImageHandle,
-                               NULL,
-                               EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    // Print(L"This isn't Done by iterating fs handles\n");
+    Status =
+        uefi_call_wrapper(BS->OpenProtocol, 5,
+                          AppLoadedImage->DeviceHandle,
+                          &gEfiSimpleFileSystemProtocolGuid,
+                          (VOID **)&RootFileSystemProtocol,
+                          ImageHandle,
+                          NULL,
+                          EFI_OPEN_PROTOCOL_GET_PROTOCOL);
 
+#ifdef FAILSAFE_BUILD
     if (Status != EFI_SUCCESS)
     {
         Print(L"Protocol is not supported - %r\n", Status);
         uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
         return Status;
     }
-
+#endif
     // Read the configuration file
 
-    CHAR16 *kernelOptions = L"root=/dev/nvme2n1p2 quiet fastboot";
+    // CHAR16 *kernelOptions = L"root=PARTUUID=0600a512-de71-6c4c-ad7c-d3326881bce6 rw quiet fastboot";
+    CHAR16 *kernelOptions = L"root=/dev/sda2 rw quiet fastboot";
     // Print(L"Loading Config File\n");
 
     // EFI_FILE_PROTOCOL *file;
@@ -113,10 +117,10 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     // }
 
     // Print(L"File size is:  %r \n", buffer_size);
-    Print(L"Config is: \n");
+    // Print(L"Config is: \n");
     // Print(L"Config is:  %r \n", buffer);
-    uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, kernelOptions);
-    Print(L"\n");
+    // uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, kernelOptions);
+    // Print(L"\n");
     // uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, buffer);
     // Print(L"\n");
     // // kernelOptions = buffer;
@@ -127,35 +131,41 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     // Load the OS Image
 
     FilePath = FileDevicePath(AppLoadedImage->DeviceHandle, KernelFileName);
-    Status = uefi_call_wrapper(BS->LoadImage,
-                               6,
-                               FALSE,
-                               ImageHandle,
-                               FilePath,
-                               (VOID *)NULL,
-                               0,
-                               &LinuxImageHandle);
+    Status =
+        uefi_call_wrapper(BS->LoadImage,
+                          6,
+                          FALSE,
+                          ImageHandle,
+                          FilePath,
+                          (VOID *)NULL,
+                          0,
+                          &LinuxImageHandle);
+#ifdef FAILSAFE_BUILD
     if (Status != EFI_SUCCESS)
     {
         Print(L"Could not load the image - %r\n", Status);
         uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
     }
-    Print(L"Loaded the image with success\n");
-    Print(L"Image start:\n");
-    Status = uefi_call_wrapper(BS->OpenProtocol, 3,
-                               LinuxImageHandle,
-                               &gEfiLoadedImageProtocolGuid,
-                               &LoadedImage,
-                               ImageHandle,
-                               NULL,
-                               EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-
+#endif
+    // Print(L"Loaded the image with success\n");
+    // Print(L"Image start:\n");
+    Status =
+        uefi_call_wrapper(BS->OpenProtocol, 3,
+                          LinuxImageHandle,
+                          &gEfiLoadedImageProtocolGuid,
+                          &LoadedImage,
+                          ImageHandle,
+                          NULL,
+                          EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+#ifdef FAILSAFE_BUILD
     if (Status != EFI_SUCCESS)
     {
         Print(L"LoadImageInfo - %r\n", Status);
         uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
     }
-    Print(L"Found Image Info \n");
+#endif
+
+    // Print(L"Found Image Info \n");
 
     CHAR16 *BootOptions = AllocatePool((StrLen(LoadedImage->LoadOptions) + StrLen(kernelOptions)) * 2 + 1);
 
@@ -165,35 +175,52 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     LoadedImage->LoadOptionsSize = StrLen(LoadedImage->LoadOptions) * 2 + 1;
 
     // uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
-    Print(L"Assigned boot options \n");
+    // Print(L"Assigned boot options \n");
+#ifdef FAILSAFE_BUILD
     if (Status != EFI_SUCCESS)
     {
         Print(L"Could not load the image params - %r %x\n", Status, Status);
         Print(L"Exit data size: %d\n", ExitDataSize);
         uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
     }
+#endif
 
-    Print(L"Options are: \n");
-    uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, LoadedImage->LoadOptions);
-    Print(L"\n");
-    Print(L"\nKernel will start in 5 seconds \n");
-    uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(5));
-    Status = uefi_call_wrapper(BS->CloseProtocol, 4,
-                               AppLoadedImage->DeviceHandle,
-                               &gEfiBlockIoProtocolGuid,
-                               ImageHandle,
-                               NULL);
+    // Print(L"Options are: \n");
+    // uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, LoadedImage->LoadOptions);
+    // Print(L"\n");
+    // Print(L"\nKernel will start in 5 seconds \n");
+    // uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(5));
+    Status =
+        uefi_call_wrapper(BS->CloseProtocol, 4,
+                          AppLoadedImage->DeviceHandle,
+                          &gEfiSimpleFileSystemProtocolGuid,
+                          ImageHandle,
+                          NULL);
+
+#ifdef FAILSAFE_BUILD
+    if (Status != EFI_SUCCESS)
+    {
+        Print(L"Could not close the bootfs - %r \n", Status);
+        Print(L"Exit data size: %d\n", ExitDataSize);
+        uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(5));
+        return Status;
+    }
+#endif
+
+    Status =
+        uefi_call_wrapper(BS->ExitBootServices, 2, ImageHandle, 0);
 
     // uefi_call_wrapper(BS->UninstallMultipleProtocolInterfaces, 4, AppLoadedImage->DeviceHandle, &gEfiBlockIoProtocolGuid, &BlockIo, NULL);
-    Status = uefi_call_wrapper(BS->ExitBootServices, 2, ImageHandle, 0);
-
-    Status = uefi_call_wrapper(BS->StartImage, 3, LinuxImageHandle, &ExitDataSize, (CHAR16 **)NULL);
+    Status =
+        uefi_call_wrapper(BS->StartImage, 3, LinuxImageHandle, &ExitDataSize, (CHAR16 **)NULL);
+#ifdef FAILSAFE_BUILD
     if (Status != EFI_SUCCESS)
     {
         Print(L"Could not start the image - %r %x\n", Status, Status);
         Print(L"Exit data size: %d\n", ExitDataSize);
         uefi_call_wrapper(BS->Stall, 1, SEC_TO_USEC(2));
     }
+#endif
 
     // Print((CHAR16 *)L"Es un pendejo amigos5!\n");
     return EFI_SUCCESS;
